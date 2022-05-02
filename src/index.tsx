34e3c13d8afddd4472bf7a6efbca1da923f63406
Elmar Amanov <Elmar.amanov10@gmail.com>
2022-05-03 00:44:07 +0400

    react-redux store

--- src/index.tsx
+++ src/index.tsx
@@ -1,42 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {Action, createStore, combineReducers} from "redux";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Action, createStore, combineReducers } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { Provider } from "react-redux";
 
-const userReducer = (state: string = "User1" , action: Action<string> & { payload: {data: string} }) => {
-  if(action.type==="new state"){return action.payload.data}
-  return state
+const userReducer = (
+  state: string = "User1",
+  action: Action<string> & { payload: { data: string } }
+) => {
+  if (action.type === "new state") {
+    return action.payload.data;
+  }
+  return state;
 };
 
-const productReducer = (state: {name: string}[] = [{name:"IPone 5C"}] , action: Action<string> & { payload: {data: {name: string }[]} }) => {
-  return state
+const productReducer = (
+  state: { name: string }[] = [{ name: "IPone 5C" }],
+  action: Action<string> & { payload: { data: { name: string }[] } }
+) => {
+  return state;
 };
 
-const rootReducer  = combineReducers({
+const rootReducer = combineReducers({
   user: userReducer,
-  products: productReducer
+  products: productReducer,
 });
 
-const store = createStore(rootReducer , composeWithDevTools() );
-
-
+const store = createStore(rootReducer, composeWithDevTools());
 
 console.log(store.getState());
-const actionDispatch:Action<string> & { payload: {data: string} } = {
+const actionDispatch: Action<string> & { payload: { data: string } } = {
   type: "new state",
-  payload: {data: "User2"}
-}
+  payload: { data: "User2" },
+};
 
-store.dispatch(actionDispatch); 
+store.dispatch(actionDispatch);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
