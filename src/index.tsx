f179abeb81b4289c3d80e655faf900fe35eab28e
Elmar Amanov <Elmar.amanov10@gmail.com>
2022-05-02 14:53:01 +0400

    React Redux STORE SUBSCRIBE

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import reportWebVitals from './reportWebVitals';
 import {Action, createStore, combineReducers} from "redux";
 
 const userReducer = (state: string = "User1" , action: Action<string> & { payload: {data: string} }) => {
+  if(action.type==="new state"){return action.payload.data}
   return state
 };
 const productReducer = (state: {name: string}[] = [{name:"IPone 5C"}] , action: Action<string> & { payload: {data: {name: string }[]} }) => {
@@ -18,8 +19,17 @@ const rootReducer  = combineReducers({
 
 const store = createStore(rootReducer);
 
-console.log(store.getState());
 
+store.subscribe(()=>{
+  console.log(store.getState());
+})
+//console.log(store.getState());
+const actionDispatch:Action<string> & { payload: {data: string} } = {
+  type: "new state",
+  payload: {data: "User2"}
+}
+
+store.dispatch(actionDispatch);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
